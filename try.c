a461fa689a50c71790d245fc2c543b95eaa27d9e
Rakeshappu <appussrh@gmail.com>
2024-08-13 23:40:55 +0530

    TextUtils

---
+++ try.c
@@ -0,0 +1,175 @@
+
+
+
+
+
+
+
+
+
+// How many times loop will get executed.
+// #include<stdio.h>
+// int main(){
+//     int i=0;
+//     while(i<=255)
+//     {
+//         printf("%d",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int a=100,b=200,c=300;
+//     if(!a>=500)
+//         b=300;
+//     c=400;
+//     printf("%d %d %d",a,b,c);
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int var1=10;
+//     {
+//         int var1=20;
+//         printf("%d %d",var1,var1);
+//     }
+//     printf("%d %d",var1,var1);
+// }
+
+
+
+
+// #include<stdio.h>
+// int x=10;
+// int main()
+// {
+//     x=0;
+//     printf("%d",x);
+//     return 0;
+// }
+
+
+
+
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int i=0;
+//     for ( i = 0; i < -5; i--)
+//     {
+//         printf("*");
+//     }
+// }
+
+
+
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int i=0;
+//     for ( i = 0; i < -5; i--)
+//     {
+//         printf("*");
+//     }
+// }
+
+
+
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int a@ = 10;
+//     printf("%d",a@);
+//     return 0;
+// }
+
+
+// Which of the following is true about linked list implementation of stack?
+
+// 1) In push operation, if new nodes are inserted at the beginning of linked list, then in pop
+//    operation, nodes must be removed from end.
+// 2) In push operation, if new nodes are inserted at the end, then in pop operation, nodes
+//    must be removed from the beginning.
+// 3) Both of the above
+// 4) None of the above
+
+
+
+// The time required to examine the packet's header and determine where to
+// direct the packet is part of
+// 1)Processing delay
+// 2)Queuing delay
+// 3)Transmission delay
+// 4)All of the mentioned
+
+
+    // Which of the following has a search efficiency of 0(1)?
+    // Tree
+    // Heap
+    // Hash table
+    // Linked list
+
+
+// Is it possible to run program without main() function?
+// Yes
+// No
+
+
+
+// If a relation is in BCNF, it is also in:
+// 1NF
+// 2NF
+// 3NF
+// All of the above
+
+
+// What part of object-oriented technology defines superclass and subclass
+// relationships?
+// Inheritance
+// Scalability
+// Encapsulation
+// Polymorphism
+
+
+// The average depth of a binary search tree is:
+// O(n^O.5)
+// O(n)
+// O(log n)
+// O(n log n)
+
+
+
+// What will be the postfix expression for following infix expression: b * c + d / e
+// b*cde/+
+// bcd*e/+
+// bc*de/+
+// bC*de+/
+
+
+
+// Which of the following is not the member of class?
+// Static function
+// Friend function
+// Const function
+// Virtual function
+
+
+
+// Which of the following is a linear data structure? Please select all that apply.
+// Array
+// Queue
+// Stack
+// All of the above
