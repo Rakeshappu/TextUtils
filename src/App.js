8d77fea0df4feb09f8eeb0ec57ebdf0f64e9fca6
Rakeshappu <appussrh@gmail.com>
2024-09-01 23:43:38 +0530

    Alert

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About'
+import Alert from './components/Alert';
+import React, {useState} from 'react';
 
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg:message,
+      type: type
+    })
+  }
+
   return (
     <>
-        <Navbar title="Case Changer"/>
+        <Navbar title="Text Utils"/>
+        <Alert alert='asdas' />
         <div className="container">
           <TextForm heading= "Enter text to change its Case"/>
+
         </div>
         {/* <About/> */}
     </>
